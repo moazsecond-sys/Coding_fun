66530354472d9927d751d0d81d1edff30885b674
Coder_lol <moazsecond@gmail.com>
2026-06-06 16:52:47 +0300

    Fix syntax error in config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // 2. حط مفاتيحك هنا بين " "
-const SUPABASE_URL = "https://mirrxytqttjglglxrarq.supabase.co"
-const SUPABASE_ANON_KEY = "sb_publishable_kuXmAppdiHaZ1OKlWsHbBg_QUebJHdu"
+const SUPABASE_URL = "https://mirrxytqttjglglxrarq.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_kuXmAppdiHaZ1OKlWsHbBg_QUebJHdu";
