c68067daa999d62d39335de6233d9bb4fff34e10
Coder_lol <moazsecond@gmail.com>
2026-06-06 21:55:02 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 // 2. حط مفاتيحك هنا بين " "
 const SUPABASE_URL = "https://mirrxytqttjglglxrarq.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_kuXmAppdiHaZ1OKlWsHbBg_QUebJHdu";
+const BUCKET = 'AAMAL-IMAGES';
